35c815185c51ba7e8ba7b5926d7a40ed493ba0a0
Nicolas de Lima <nicolasdaniel2106@gmail.com>
2024-03-12 21:03:30 -0400

    adding play_again loop and fixing chars bugs

--- guess_the_number.c
+++ guess_the_number.c
@@ -5,23 +5,55 @@
 #define RANGE 10
 
 int main() {
-    srand(time(NULL));
-    int sorted_num = rand() % RANGE;
+    
+    while (true) {
+        srand(time(NULL));
+        int sorted_num = rand() % RANGE;
 
-    while(true) {
-        int guess;
+        while(true) {
+            int guess;
 
-        printf("Guess the sorted number from 1 to %d: ", RANGE);
-        while(!scanf("%d",&guess)) {
-            printf("That's not a number, dumbass. Try it again: ");
-            getchar();
+            //system("color 60");
+            printf("Guess the sorted number from 1 to %d: ", RANGE);
+
+            while(!scanf("%d",&guess)) {
+                // system("color 64");
+                printf("That's not a number, dumbass. Try it again: "); //okay
+                getchar();
+            }
+
+            
+            if (guess == sorted_num) {
+                // system("color 6f");
+                system("cls");
+                printf("CONGRATS! You've guessed the number.\n\n");
+                break;
+            } else {
+                // system("color 64");
+                printf("WRONG!\n");
+            }
         }
 
-        if (guess == sorted_num) {
-            printf("CONGRATS! You've guessed the number.");
+        int option;
+        while (true) {
+            // system("color 60");
+            printf("\t(0) Play again\n\t(1) Exit\n\tOption: ");
+            int dummy = scanf("%d", &option);
+            //!scanf("%d", &option) || (option != 0 && option != 1);
+            
+            if ((option == 1 || !option) && dummy)
+                break;
+
+            // system("color 60");
+            getchar();
+            printf("\nThat's not an option.\n\n");
+        }
+
+        if (option) {
+            printf("\nExiting program...\n");
             break;
         } else
-            printf("WRONG!\n");
+            system("cls");  
     }
 
     system("pause");
